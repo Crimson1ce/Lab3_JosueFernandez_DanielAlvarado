8fa6bddf7edcb4ad56ba87444612e447c6010de1
Daniel A. Alvarado <danielalvarado019@gmail.com>
2020-08-07 14:47:11 -0600

    Clase Prueba

--- Lab3_JosueFernandez_DanielAlvarado/src/laboratorio3_josuefernandez_danielalvarado/Prueba.java
+++ Lab3_JosueFernandez_DanielAlvarado/src/laboratorio3_josuefernandez_danielalvarado/Prueba.java
@@ -5,10 +5,66 @@
  */
 package laboratorio3_josuefernandez_danielalvarado;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author dalva
  */
 public class Prueba {
-    
+    private String nombre;
+    private Ranker evaluador;
+    private ArrayList<Normal> participantes = new ArrayList();
+    private boolean aprovada;
+
+    public Prueba() {
+    }
+
+    public Prueba(String nombre, Ranker evaluador) {
+        this.nombre = nombre;
+        this.evaluador = evaluador;
+    }
+
+    public Prueba(String nombre, Ranker evaluador, boolean aprovada) {
+        this.nombre = nombre;
+        this.evaluador = evaluador;
+        this.aprovada = aprovada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Ranker getEvaluador() {
+        return evaluador;
+    }
+
+    public void setEvaluador(Ranker evaluador) {
+        this.evaluador = evaluador;
+    }
+
+    public ArrayList<Normal> getParticipantes() {
+        return participantes;
+    }
+
+    public void setParticipantes(ArrayList<Normal> participantes) {
+        this.participantes = participantes;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+    @Override
+    public String toString() {
+        return "Prueba: " + "nombre = " + nombre + "  evaluador = " + evaluador + ", participantes = " + participantes + ", aprovada = " + aprovada;
+    }
 }
