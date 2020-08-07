c083de4b70b57b00f438bf36c741921627827c61
Josue <davyd3501@gmail.com>
2020-08-07 16:08:29 -0600

    Terminado metodo crearPersona[F

--- Lab3_JosueFernandez_DanielAlvarado/src/laboratorio3_josuefernandez_danielalvarado/Laboratorio3_JosueFernandez_DanielAlvarado.java
+++ Lab3_JosueFernandez_DanielAlvarado/src/laboratorio3_josuefernandez_danielalvarado/Laboratorio3_JosueFernandez_DanielAlvarado.java
@@ -3,6 +3,7 @@
  */
 package laboratorio3_josuefernandez_danielalvarado;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -16,7 +17,8 @@ public class Laboratorio3_JosueFernandez_DanielAlvarado {
     
     static boolean signedInAsAdministrator = false;
     
-    //static ArrayList<Piso> = new ArrayList();
+    static ArrayList<Piso> torre = new ArrayList();
+    static ArrayList<Integer> pisosExistentes = new ArrayList();
     
     /**
      * @param args the command line arguments
@@ -130,6 +132,19 @@ public class Laboratorio3_JosueFernandez_DanielAlvarado {
     }
 
     public static void crearPersona(){
+        
+        //Listar Pisos
+        ////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+        System.out.print("Ingrese el piso en que desea agregar esta persona: ");
+        int cualPiso = sc.nextInt();
+        
+        while(cualPiso<1 || cualPiso>134 || !pisosExistentes.contains(cualPiso)){
+            System.out.print("\tEl piso ingresado no es válido, ingrese de nuevo: ");
+        }   cualPiso = sc.nextInt();
+        
         //Crear persona
         System.out.println("***CREAR PERSONA***\n");
         System.out.print("Ingrese el nombre de la persona: ");
@@ -143,7 +158,9 @@ public class Laboratorio3_JosueFernandez_DanielAlvarado {
         System.out.println("5-> Manipulador de ondas");
         System.out.print("\nIngrese la posición de la persona: ");
         int posi = sc.nextInt();
+        System.out.println();
         
+        //Validación de que sea una posición válida
         while(posi<1 || posi>5){
             
             System.out.println("\tPosiciones:");
@@ -154,9 +171,112 @@ public class Laboratorio3_JosueFernandez_DanielAlvarado {
             System.out.println("\t5-> Manipulador de ondas");
             System.out.print("\n\tIngrese la posición de la persona: ");
             posi = sc.nextInt();
-            
+            System.out.println();
         }
         
-    }
+        String posicion="";
+        switch(posi){
+            case 1:
+                posicion = "Pescador";
+                break;
+            case 2:
+                posicion = "Portador de lanzas";
+                break;
+            case 3:
+                posicion = "Portador de luz";
+                break;
+            case 4:
+                posicion = "Explorador";
+                break;
+            case 5:
+                posicion = "Manipulador de ondas";
+        }
+        
+        System.out.print("Ingrese la identificación de la persona: ");
+        int id = sc.nextInt();
+        
+        for (int i=0; i<torre.size(); i++) {
+            
+            for (Persona h : torre.get(i).getHabitantes()) {
+                //Conseguimos el id de cada persona de cada piso de la torre
+                int identificacion = h.getIdentificacion();
+                //Lo comparamos con el nuevo id
+                if (identificacion==id) {
+                    System.out.print("La identificación ingresada no stá disponible. Ingrese una diferente: ");
+                    id = sc.nextInt();
+                    i=-1;
+                }
+            }
+        }
+        
+        System.out.println("\nTipos:");
+        System.out.println("1-> Normal");
+        System.out.println("2-> Ranker");
+        System.out.print("Ingrese el tipo de persona que es: ");
+        int tipo = sc.nextInt();
+        System.out.println();
+        
+        //Validación de que sea una posición válida
+        while(tipo<1 || tipo>2){
+            
+            System.out.println("\tTipos:");
+            System.out.println("\t1-> Normal");
+            System.out.println("\t2-> Ranker");
+            System.out.print("\n\tIngrese un tipo de persona válido: ");
+            tipo = sc.nextInt();
+            System.out.println();
+        }
+        
+        System.out.println("\nEstado de registro:");
+        System.out.println("1-> Regular");
+        System.out.println("2-> Irregular");
+        System.out.print("\nIngrese el estado de registro de la persona: ");
+        int registro = sc.nextInt();
+        System.out.println();
+        
+        //Validación de que sea una posición válida
+        while(registro<1 || registro>2){
+            
+            System.out.println("\tEstado de registro:");
+            System.out.println("\t1-> Regular");
+            System.out.println("\t2-> Irregular");
+            System.out.print("\n\tIngrese un estado de registro válido: ");
+            tipo = sc.nextInt();
+            System.out.println();
+        }
+        
+        int pisoEnLista=0;
+        for (int i = 0; i < torre.size(); i++) {
+            if (cualPiso==torre.get(i).getNivel()) {
+                pisoEnLista = i;
+            }
+        }
+        
+        //Creamos el estado de registro
+        EstadoRegistro estado;
+                
+        //En caso de ser regular
+        if (registro==1) {
+            System.out.print("Cree un usuario: ");
+            String usuario = sc.next();
+            System.out.print("Cree una contraseña: ");
+            String contraseña = sc.next();
+            estado = new Regular(usuario, posicion);
+        } else {
+            estado = new Irregular();
+        }
+        
+        //En caso de ser normal...
+        if(tipo==1){
+            System.out.print("Escriba el obetivo de la persona: ");
+            String objetivo = sc.next();
+                
+            //Creamos la persona
+            torre.get(pisoEnLista).getHabitantes().add(new Normal(objetivo, nombre, id, new Posicion(posicion), estado));
+        } else {
+            torre.get(pisoEnLista).getHabitantes().add(new Ranker(nombre, id, new Posicion(posicion), estado));
+        }
+        
+    }//Fin de crearPersona
     
 }
