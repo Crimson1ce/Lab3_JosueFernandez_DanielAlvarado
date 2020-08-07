c94be84c83d90abdd5cc93bfb07420ce68379ee8
Daniel A. Alvarado <danielalvarado019@gmail.com>
2020-08-07 14:53:41 -0600

    Estado de Registro + subClases

---
+++ Lab3_JosueFernandez_DanielAlvarado/src/laboratorio3_josuefernandez_danielalvarado/Persona.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio3_josuefernandez_danielalvarado;
+
+public class Persona {
+    
+    private String nombre;
+    private int identificacion;
+    private String posicion;
+    private String [] Datos = new String [2];
+
+    public Persona() {
+    }
+}
